2ffbe1e6e8a34770c7c16ac3ab8edb4eb2a495bf
cristian-nambo <cristian-nambo@hotmail.com>
2023-05-10 20:55:20 -0700

    added event listeners for the arrow keys

--- snake.js
+++ snake.js
@@ -12,6 +12,9 @@ var context;
 var snakeX = blockSize * 5; //this is the starting point of the snake row 5 coloumn 5
 var snakeY = blockSize * 5;
 
+var velocityX = 0;
+var velocityY = 0;
+
 //FOOD
 var foodX; //GOT RID OF THE STARTING POINTS SINCE WE CAN NOW CALL THEM AT RANDOM.
 var foodY;
@@ -23,7 +26,9 @@ window.onload = function(){
     context = board.getContext("2d"); //USED FOR DRAWING ON THE BOARD
 
     placeFood();
-    update();
+    document.addEventListener("keyup", changeDirection); 
+    //update(); is being called once. but we need to call it multimple times
+    setInterval (update, 1000/10); // every 100 miliseconds it will run the update function.
 }
 
 function update() {
@@ -31,15 +36,41 @@ function update() {
     context.fillRect(0, 0, board.width, board.height); //THIS IS THE COLOR OF THE BOARD AND ITS PROPERTIES
 
     context.fillStyle = "lime";
+    snakeX += velocityX * blockSize; 
+    snakeY += velocityY * blockSize; //giving the snake speed from the function below
     context.fillRect(snakeX, snakeY, blockSize, blockSize); //x and y coordinates this gives color to the snake
 
     context.fillStyle = "red";
     context.fillRect(foodX, foodY, blockSize, blockSize); //This is giving the food its color and properties
 
 }
-//WE NEED A FUNSTION THAT WILL PLACE THE FOOD AT A RANDOM PLACE USING MATH
-function placeFood(){
-    //0-1) *cols ->(0-19.99999) ->(0-19) *25
+
+//function to chage the direction of the snake using the arrow keys
+
+function changeDirection(e){
+    if(e.code == "ArrowUp"){
+        velocityX = 0;
+        velocityY = -1;
+    }
+    else if(e.code == "ArrowDown"){
+        velocityX = 0;
+        velocityY = 1;
+    }
+    if(e.code == "ArrowLeft"){
+        velocityX = -1;
+        velocityY = 0;
+    }
+    if(e.code == "ArrowRight"){
+        velocityX = 1;
+        velocityY = 0;
+    }
+}
+
+//WE NEED A FUNSTION THAT WILL PLACE THE FOOD AT A RANDOM PLACE USING MATH.random RETURNS A NUMBER BETWEEN 0-1 AND 
+//WE ARE MULTIPLYING BY THE NUMBER OF COLUMS OR ROWS. THEN WE DO MATH.FLOOR SO WE CAN GET RID OF THE DECIMAL FINALLY MULTIPLIED BY THE BLOCKSIZE
+//0-1) *cols =>(0-19.99999) =>(0-19) *25
+
+function placeFood(){ 
     foodX = Math.floor(Math.random() * cols) * blockSize
     foodY = Math.floor(Math.random() * rows) * blockSize
 }
